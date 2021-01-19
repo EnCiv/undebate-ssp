75a483909d5bfaf0e423b00361966481f528be7f
David Fridley <ddfridley@yahoo.com>
2021-01-19 14:32:04 -0800

    more doc in postinstall

--- postinstall.sh
+++ postinstall.sh
@@ -5,7 +5,7 @@ echo POSTINSTALL
 echo '*************************************************************************'
 
 #echo '*************************************************************************'
-#echo Symbolick link
+#echo These directoies need to exist in dist, even if you don't have them in your project
 #echo '*************************************************************************'
 
 mkdir -p dist/events
@@ -13,7 +13,10 @@ mkdir -p dist/routes
 mkdir -p dist/socket-apis
 mkdir -p dist/web-components
 #
+# assets is where static files go
+#
 mkdir -p ./assets/js/
+# socket.io-streams requires a static load of this file, so we put it in assets
 cp ./node_modules/socket.io-stream/socket.io-stream.js ./assets/js/
 # you can start with the favicon images from civil-server - but you may want to replace them with your own some day
 mkdir -p ./assets/images
