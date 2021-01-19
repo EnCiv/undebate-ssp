2a718ee33f96502d209ac16f33b62811d360c023
David Fridley <ddfridley@yahoo.com>
2021-01-19 13:53:13 -0800

    civil-server update, Home component added

---
+++ app/web-components/home.js
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from "react"
+
+export default function Home(props){
+    const {subject, description}=props
+    return (
+    <div style={{width: '100vw', height: '100vh'}}>
+        <div style={{textAlign: 'center'}}>{subject}</div>
+        <div style={{textAlign: 'center'}}>{description}</div>
+        <div style={{textAlign: 'center'}}>Welcome!</div>
+    </div>
+    )
+}
