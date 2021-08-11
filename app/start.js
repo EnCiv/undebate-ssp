55475f9605fcadcc5e9d71b884b67ca39a678ccd
David Fridley <ddfridley@yahoo.com>
2021-08-11 15:04:59 -0700

    now includes undebate as a component

--- app/start.js
+++ app/start.js
@@ -3,11 +3,13 @@
 var civilServer= require('civil-server').default
 const path=require('path')
 import {Iota} from "civil-server"
-import moduleIotas from '../node_modules/civil-server/iotas.json'
+import civilIotas from '../node_modules/civil-server/iotas.json'
+import undebateIotas from '../node_modules/undebate/iotas.json'
 import iotas from '../iotas.json'
 import App from './components/app'
 
-Iota.load(moduleIotas)
+Iota.load(civilIotas)
+Iota.load(undebateIotas)
 Iota.load(iotas) // set the initial data for the database
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 async function start() {
@@ -15,8 +17,11 @@ async function start() {
     const server = new civilServer()
     server.App=App // set the outer React wrapper for this site
     await server.earlyStart() // connect to the database, and such
+    server.routesDirPaths.push(path.resolve(__dirname, '../node_modules/undebate/dist/routes'))
     server.routesDirPaths.push(path.resolve(__dirname, './routes'))
+    server.socketAPIsDirPaths.push(path.resolve(__dirname, '../node_modules/undebate/dist/socket-apis'))
     server.socketAPIsDirPaths.push(path.resolve(__dirname, './socket-apis'))
+    server.serverEventsDirPaths.push(path.resolve(__dirname, '../node_modules/undebate/dist/events'))
     server.serverEventsDirPaths.push(path.resolve(__dirname, './events'))
     await server.start()
     logger.info("started")
