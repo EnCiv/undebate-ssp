607aba4aaccd0e932fffd06d25b3072537557fd6
Bolun Thompson <abolunthompson@gmail.com>
2021-09-20 18:59:43 -0700

    add definitions file for svgs not included in repo

---
+++ app/components/svg.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export { default as SvgAccepted } from "../svgr/accepted.js";
+export { default as SvgDeadlineMissed } from "../svgr/deadline-missed.js";
+export { default as SvgDeclined } from "../svgr/declined.js";
+export { default as SvgCompleted } from "../svgr/completed.js";
+export { default as SvgReminderSent } from "../svgr/reminder-sent.js";
+export { default as SvgSent } from "../svgr/sent.js";
+export { default as SvgVideoSubmitted } from "../svgr/video-submitted.js";
