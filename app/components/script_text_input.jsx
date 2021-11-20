cf1849cedf7f5e58bdef4ddd6ad5c7c0b4bacf2c
Kamui <fin-kamui@pm.me>
2021-11-20 16:02:43 -0600

    feat: add script text input

---
+++ app/components/script_text_input.jsx
@@ -0,0 +1,130 @@
+"use-strict"
+
+// https://github.com/EnCiv/undebate-ssp/issues/10
+
+import React, { useState } from "react"
+import { createUseStyles } from "react-jss"
+import SvgVideo from "../svgr/video"
+import SvgClock from "../svgr/clock"
+import TextareaAutosize from "react-textarea-autosize"
+import moment from "moment"
+
+export const ScriptTextInput = ({
+    questionNumber,
+    questionName,
+    maxWordCount,
+    wordsPerMinute,
+    defaultValue,
+    onDone,
+}) => {
+    const classes = useStyles()
+    const [inputText, setInputText] = useState(defaultValue)
+
+    const getWordCount = () => {
+        return inputText.split(" ").filter(word => {
+            return word != ""
+        }).length
+    }
+
+    const [wordCount, setWordCount] = useState(getWordCount())
+
+    const formatDuration = minutes => {
+        const dur = moment.duration(minutes, "minutes")
+        const formatted = moment.utc(dur.asMilliseconds()).format("mm:ss")
+        return formatted
+    }
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.scriptInfo}>
+                <div className={classes.question}>
+                    <SvgVideo />
+                    <h5 className={classes.questionNum}>{questionNumber}</h5>
+                    <h5 className={classes.questionName}>{questionName}</h5>
+                </div>
+                <div className={classes.quantity}>
+                    <div className={classes.time}>
+                        <span className={classes.duration}>{formatDuration(wordCount / wordsPerMinute)}</span>
+                        <SvgClock />
+                    </div>
+                    <div className={classes.words}>
+                        <span>
+                            {wordCount}/{maxWordCount}
+                        </span>
+                    </div>
+                </div>
+            </div>
+            <TextareaAutosize
+                minRows={4}
+                className={classes.input}
+                onChange={event => {
+                    setInputText(event.target.value)
+                    setWordCount(getWordCount())
+                }}
+                onBlur={() => {
+                    onDone({
+                        value: inputText,
+                        valid: wordCount <= maxWordCount,
+                    })
+                }}
+            >
+                {inputText}
+            </TextareaAutosize>
+        </div>
+    )
+}
+
+const useStyles = createUseStyles({
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        fontFamily: "sans-serif",
+        color: "#262D33",
+    },
+    scriptInfo: {
+        display: "flex",
+        justifyContent: "space-between",
+        padding: ".625rem",
+        fontSize: "0.875rem",
+    },
+    question: {
+        display: "flex",
+        alignItems: "center",
+        gap: ".4rem",
+    },
+    questionNum: {
+        margin: 0,
+    },
+    questionName: {
+        margin: "0rem 0rem 0rem .4rem",
+        fontSize: "0.875rem",
+    },
+    quantity: {
+        display: "flex",
+        alignItems: "center",
+    },
+    time: {
+        display: "flex",
+        alignItems: "center",
+        gap: "0.438rem",
+    },
+    duration: {
+        opacity: "70%",
+    },
+    words: {
+        opacity: "70%",
+        marginLeft: "2.5rem",
+    },
+    input: {
+        opacity: "70%",
+        background: "linear-gradient(0deg, rgba(38, 45, 51, 0.2), rgba(38, 45, 51, 0.2)), #FFFFFF",
+        padding: "0.938rem",
+        resize: "none",
+        border: "none",
+        borderRadius: "0.438rem",
+        fontSize: "1rem",
+        fontWeight: "500",
+        lineHeight: "1.875rem",
+        margin: "0.375rem 0rem",
+    },
+})
---
+++ stories/script_text_input.stories.js
@@ -0,0 +1,25 @@
+// https://github.com/EnCiv/undebate-ssp/issues/10
+
+import React from "react"
+
+import { ScriptTextInput } from "../app/components/script_text_input"
+
+export default {
+    title: "Script Text Input",
+    component: ScriptTextInput,
+    argTypes: {},
+}
+
+const Template = args => <ScriptTextInput {...args} />
+
+export const ScriptTextInputTest = Template.bind({})
+
+ScriptTextInputTest.args = {
+    questionNumber: 1,
+    questionName: "Moderator welcomes the viewers and asks the candidates to introduce themselves.",
+    maxWordCount: 600,
+    wordsPerMinute: 100,
+    defaultValue:
+        "Neque a massa nulla tortor quam. Eget massa facilisis tortor dui ullamcorper enim, quis enim. Neque mi elementum, blandit laoreet. Arcu ut id tortor diam malesuada. Adipiscing eros, nec amet nulla condimentum enim tempor. Non sed interdum convallis bibendum morbi sagittis feugiat aliquet. Tempor ut massa purus et nec nulla. Nulla vitae turpis quis aliquam ornare nisi etiam. In dui amet viverra aliquet neque. Nunc ut felis ridiculus nec convallis. Vitae molestie augue malesuada nulla cursus ut donec quisque tincidunt. ”Et non non sem ac a, sapien. Sed tellus senectus magna” lectus eu habitant. Viverra iaculis ac sagittis amet, pellentesque duis eget etiam. Mattis sed dictum id turpis. Egestas sagittis, facilisis scelerisque mattis. Adipiscing tortor, pretium sed egestas convallis ultrices nec. ”Feugiat pretium semper lorem integer bibendum.” Viverra ut purus amet purus odio lobortis facilisis id diam. Convallis mi porttitor accumsan non hac et nisl. Mus amet non nec mattis massa.",
+    onDone: null,
+}
