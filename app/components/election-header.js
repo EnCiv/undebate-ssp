39b16dfd89710571625d318e4c43e81de2b823f3
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2021-11-27 02:39:22 -0300

    add styles and layout, initiate the props

--- app/components/election-header.js
+++ app/components/election-header.js
@@ -3,23 +3,48 @@
 
 import React from "react";
 import SvgBookOpen from '../svgr/book-open'
-import SvgChevronDown from '../svgr/chevron-down'
 import SvgChevronLeft from '../svgr/chevron-left'
 import SvgHome from '../svgr/home'
 import { createUseStyles } from "react-jss";
 
 const ElectionHeader = (props) => {
-    // const classes = useStyles();
-  
-    return (
+  const classes = useStyles();
+  const {
+    defaultValue = 0,
+    elections = [],
+    onDone = () => {},
+  } = props;
+
+  return (
+    <div className={classes.electionHeader}>
       <div>
-          <SvgChevronLeft />
-          <SvgHome />
-          Election Header Select
-          <SvgChevronDown />
-          <SvgBookOpen />
+        <SvgChevronLeft />
+        <SvgHome />
       </div>
-    );
+      <select className={classes.electionSelect}>
+        {elections.map(element => <option>{element}</option>)}
+      </select>
+      <div>
+        <SvgBookOpen />
+      </div>
+    </div>
+  );
 };
 
 export default ElectionHeader;
+
+const useStyles = createUseStyles({
+  electionHeader: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "1.25rem 2.5rem",
+  },
+  electionSelect: {
+    border: "none",
+    fontWeight: "bold",
+    "&:hover": {
+      cursor: "pointer",
+    }
+  }
+});
--- stories/election-header.stories.js
+++ stories/election-header.stories.js
@@ -3,12 +3,18 @@
 import ElectionHeader from "../app/components/election-header";
 
 export default {
-    title: "Election Header",
-    component: ElectionHeader,
-  };
+  title: "Election Header",
+  component: ElectionHeader,
+};
 
 const Template = (args) => <ElectionHeader {...args} />;
 
 export const ElectionHeaderTest = Template.bind({});
-ElectionHeaderTest.args = {};
+ElectionHeaderTest.args = {
+  elections : [
+    'District Attorney Election 1',
+    'District Attorney Election 2',
+    'District Attorney Election 3'
+  ]
+};
 
