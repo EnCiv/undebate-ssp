f487716d33ebca340596e8f74bb6db31be8ca2a6
Kamui <fin-kamui@pm.me>
2021-12-23 17:00:24 -0600

    chore: eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["plugin:react/recommended", "airbnb", "plugin:prettier/recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: "module",
+    },
+
+    plugins: ["react", "prettier"],
+    rules: {
+        "prettier/prettier": "warn",
+        "react/jsx-filename-extension": [1, { extensions: [".js"] }],
+    },
+}
