e94fcfc8e2059d74bf2f3dfaa4d85458c834264f
tngan8519 <nganthithaotran@gmail.com>
2022-01-07 11:15:43 -0600

    add count limited text input issue 8

---
+++ app/components/count-limited-text-input#8.js
@@ -0,0 +1,83 @@
+// https://github.com/EnCiv/undebate-ssp/issues/8
+import React, { useState } from 'react'
+import { createUseStyles } from 'react-jss'
+import TextareaAutosize from 'react-textarea-autosize'
+
+export default function CountLimitedTextInput({ name, maxCount, defaultValue, onDone }) {
+    const classes = useStyles()
+    const [inputText, setInputText] = useState(defaultValue)
+
+    const getWordCount = () => {
+        return inputText.split(' ').filter(word => {
+            return word !== '' && word !== '\n'
+        }).length
+    }
+
+    const [wordCount, setWordCount] = useState(getWordCount())
+
+    const updateState = event => {
+        setInputText(event.target.value)
+        setWordCount(getWordCount())
+    }
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.scriptInfo}>
+                <div className={classes.question}>{name}</div>
+                <div className={classes.quantity}>
+                    ({wordCount}/{maxCount})
+                </div>
+            </div>
+            <TextareaAutosize
+                className={classes.input}
+                onChange={event => {
+                    updateState(event)
+                }}
+                onBlur={event => {
+                    updateState(event)
+                    onDone({
+                        value: inputText,
+                        valid: wordCount <= maxCount && wordCount >= 1,
+                    })
+                }}
+            >
+                {inputText}
+            </TextareaAutosize>
+        </div>
+    )
+}
+
+const useStyles = createUseStyles({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        fontFamily: 'Poppins',
+        color: '#262D33',
+    },
+    scriptInfo: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '.625rem',
+    },
+    question: {
+        display: 'flex',
+        alignItems: 'center',
+        fontWeight: '600',
+    },
+    quantity: {
+        display: 'flex',
+        alignItems: 'center',
+        fontSize: '0.875rem',
+        opacity: '70%',
+    },
+    input: {
+        background: 'linear-gradient(0deg, rgba(38, 45, 51, 0.2), rgba(38, 45, 51, 0.2)), #FFFFFF',
+        padding: '0.9375rem 1.25rem',
+        resize: 'none',
+        border: 'none',
+        borderRadius: '0.625rem',
+        fontSize: '1rem',
+        margin: '0.375rem 0rem',
+        opacity: '80%',
+    },
+})
