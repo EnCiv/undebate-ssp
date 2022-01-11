d300653d18aef61d7fd39e360745a66047f9aace
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-11 15:52:25 -0800

    Add initial theme constants

--- app/theme.js
+++ app/theme.js
@@ -1,5 +1,21 @@
 const theme = {
+    // UI Colors
     colorPrimary: 'linear-gradient(0deg, #7470FF, #7470FF), #FFFFFF',
+    colorSecondary: '#262D33',
+    colorWarning: '#EE6055',
+    colorSuccess: '#46941B',
+
+    // Background Colors
+    backgroundColorApp: '#ECECEC',
+    backgroundColorWarning: 'rgba(238, 96, 85, 0.25)',
+    backgroundColorComponent: 'linear-gradient(0deg, rgba(38, 45, 51, 0.2), rgba(38, 45, 51, 0.2)), #FFFFFF',
+
+    // Sizing
+    iconSize: '1.25rem',
+    buttonPadding: '1rem 1.875rem',
+    defaultPadding: '1.5rem',
+    buttonBorderRadius: '1.875rem',
+    defaultBorderRadius: '0.625rem',
 }
 
 export default theme
