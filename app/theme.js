40a0e6b98c53f26528791d2073ba874788bec818
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-19 13:10:54 -0800

    Add and simplify theme values

--- app/theme.js
+++ app/theme.js
@@ -1,6 +1,6 @@
 const theme = {
     // UI Colors
-    colorPrimary: 'linear-gradient(0deg, #7470FF, #7470FF), #FFFFFF',
+    colorPrimary: '#7470FF',
     colorSecondary: '#262D33',
     colorWarning: '#EE6055',
     colorSuccess: '#46941B',
@@ -8,14 +8,20 @@ const theme = {
     // Background Colors
     backgroundColorApp: '#ECECEC',
     backgroundColorWarning: 'rgba(238, 96, 85, 0.25)',
-    backgroundColorComponent: 'linear-gradient(0deg, rgba(38, 45, 51, 0.2), rgba(38, 45, 51, 0.2)), #FFFFFF',
+    backgroundColorComponent: 'rgba(38, 45, 51, 0.2)',
 
     // Sizing
-    iconSize: '1.25rem',
-    buttonPadding: '1rem 1.875rem',
-    inputFieldPadding: '1rem 1.25rem',
-    buttonBorderRadius: '1.875rem',
     defaultBorderRadius: '0.625rem',
+    iconSize: '1.25rem',
+
+    buttonPadding: '1rem 1.875rem',
+    buttonBorderRadius: '1.875rem',
+
+    inputFieldPadding: '1rem 1.25rem',
+    inputFieldFontSize: '1.125rem',
+
+    //Font
+    defaultFontFamily: 'Poppins',
 }
 
 export default theme
