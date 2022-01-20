92224124fb7d80c55ed80a2df49400c222bc5d6d
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-20 11:24:29 -0800

    Fix formatting

--- app/components/timeline-point.js
+++ app/components/timeline-point.js
@@ -1,19 +1,22 @@
-import { React, useEffect, useReducer } from "react"
+import { React, useEffect, useReducer } from 'react'
 
-import ElectionTimeInput from "./election-time-input"
-import ElectionDateInput from "./election_date_input"
+import ElectionTimeInput from './election-time-input'
+import ElectionDateInput from './election-date-input'
 
-const TimelinePoint = props => {
-    const { className, style, title, description, dateTimes = [], onDone = () => {}, ref } = props
+const TimelinePoint = function (props) {
+    const { className, style, electionOM, title, description, dateTimes = [], onDone = () => {}, ref } = props
+    const [electionObj, electionMethods] = electionOM
 
     // onDone is if we receive 'dones' from all input fields
     // 1. Keep track of the number of 'dones' received as state, each time a new 'done' is received, check if we have all those necessary
     // 2. State could just be the current date times and their validity and we could just go through all of them to check if they are valid
     const reducer = (state, action) => {
         switch (action.type) {
-            case "valid":
+            case 'valid':
                 return [...state, action.payload]
-            case "invalid":
+            case 'invalid':
+                return state
+            default:
                 return state
         }
     }
@@ -35,13 +38,13 @@ const TimelinePoint = props => {
                         <ElectionDateInput
                             defaultValue={date}
                             onDone={({ valid, value }) =>
-                                dispatch(state, { type: valid ? "valid" : "invalid", payload: value })
+                                dispatch(state, { type: valid ? 'valid' : 'invalid', payload: value })
                             }
                         />
                         <ElectionTimeInput
                             defaultValue={time}
                             onDone={({ valid, value }) =>
-                                dispatch(state, { type: valid ? "valid" : "invalid", payload: value })
+                                dispatch(state, { type: valid ? 'valid' : 'invalid', payload: value })
                             }
                         />
                     </div>
--- stories/timeline-point.stories.js
+++ stories/timeline-point.stories.js
@@ -1,34 +1,35 @@
-import TimelinePoint from "../app/components/timeline-point"
+import React from 'react'
+import TimelinePoint from '../app/components/timeline-point'
 
 export default {
-    title: "Timeline Point",
+    title: 'Timeline Point',
     component: TimelinePoint,
 }
 
 const Template = args => (
-    <div style={{ width: "50%" }}>
+    <div style={{ width: '50%' }}>
         <TimelinePoint {...args} />
     </div>
 )
 
 export const Default = Template.bind({})
 Default.args = {
-    title: "Moderator Deadline Reminder Emails",
+    title: 'Moderator Deadline Reminder Emails',
     description:
-        "Moderator will recieve two emails as a reminder on this date, usually 2 dats and 7 days before the deadline.",
+        'Moderator will recieve two emails as a reminder on this date, usually 2 dats and 7 days before the deadline.',
     dateTimes: [
-        { date: "", time: "" },
-        { date: "", time: "" },
+        { date: '', time: '' },
+        { date: '', time: '' },
     ],
 }
 
 export const Filled = Template.bind({})
 Filled.args = {
-    title: "Moderator Deadline Reminder Emails",
+    title: 'Moderator Deadline Reminder Emails',
     description:
-        "Moderator will recieve two emails as a reminder on this date, usually 2 dats and 7 days before the deadline.",
+        'Moderator will recieve two emails as a reminder on this date, usually 2 dats and 7 days before the deadline.',
     dateTimes: [
-        { date: "11/26/21", time: "14:00" },
-        { date: "11/27/21", time: "02:00" },
+        { date: '11/26/21', time: '14:00' },
+        { date: '11/27/21', time: '02:00' },
     ],
 }
