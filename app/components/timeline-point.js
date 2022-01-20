b8e6efa738533b6250afb83b9fd5ba5c79503c57
Alex Davis <63484411+rawbeef-808@users.noreply.github.com>
2022-01-20 14:22:18 -0800

    Change state shape

--- app/components/timeline-point.js
+++ app/components/timeline-point.js
@@ -5,16 +5,19 @@ import ElectionDateInput from './election-date-input'
 
 const TimelinePoint = function (props) {
     const { className, style, electionOM, title, description, dateTimes = [], onDone = () => {}, ref } = props
-    const [electionObj, electionMethods] = electionOM
 
     // onDone is if we receive 'dones' from all input fields
     // 1. Keep track of the number of 'dones' received as state, each time a new 'done' is received, check if we have all those necessary
     // 2. State could just be the current date times and their validity and we could just go through all of them to check if they are valid
     const reducer = (state, action) => {
+        console.log('action: ', action)
         switch (action.type) {
             case 'valid':
-                return [...state, action.payload]
+                const entry = { [`dateTime${action.payload.id}`]: action.payload.value }
+                return { ...state, ...entry }
             case 'invalid':
+                // if it has become invalid, it must be removed from the state
+                // if it is just initially invalid, in just must not be added
                 return state
             default:
                 return state
@@ -22,9 +25,14 @@ const TimelinePoint = function (props) {
     }
 
     useEffect(() => {
-        console.log(state)
-    }, [state])
-    const [state, dispatch] = useReducer(reducer, [])
+        console.log('current state:', state)
+        if (state.length === dateTimes.length * 2) {
+            console.log('done')
+        } else {
+            console.log('not done')
+        }
+    })
+    const [state, dispatch] = useReducer(reducer, {})
 
     //onDone({ valid: true, dateTimes: [] })
 
@@ -32,20 +40,23 @@ const TimelinePoint = function (props) {
         <div ref={ref}>
             <div>{title}</div>
             <div>{description}</div>
-            {dateTimes.map(({ date, time }) => {
+            {dateTimes.map(({ date, time }, i) => {
                 return (
                     <div>
                         <ElectionDateInput
                             defaultValue={date}
                             onDone={({ valid, value }) =>
-                                dispatch(state, { type: valid ? 'valid' : 'invalid', payload: value })
+                                dispatch({
+                                    type: valid ? 'valid' : 'invalid',
+                                    payload: { value: value, id: i },
+                                })
                             }
                         />
                         <ElectionTimeInput
                             defaultValue={time}
-                            onDone={({ valid, value }) =>
-                                dispatch(state, { type: valid ? 'valid' : 'invalid', payload: value })
-                            }
+                            onDone={({ valid, value }) => {
+                                dispatch({ type: valid ? 'valid' : 'invalid', payload: value })
+                            }}
                         />
                     </div>
                 )
