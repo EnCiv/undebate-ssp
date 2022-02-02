5494cc46e0895940c7d3baa560b335de92323abe
David Fridley <ddfridley@yahoo.com>
2022-02-01 20:09:13 -0800

    notes from our call

--- app/socket-apis/upsert-election-doc.js
+++ app/socket-apis/upsert-election-doc.js
@@ -11,50 +11,28 @@ const schema = Joi.object({
     _id: ID,
     subject: Joi.string(),
     description: Joi.string(),
-    electionName: Joi.string(),
-    organizationName: Joi.string(),
-    electionDate: Joi.string(),
-    questions: Joi.object().pattern(
-        Joi.string(),
-        Joi.object({
-            text: Joi.string(),
-            time: Joi.string(),
-        })
-    ),
-    script: Joi.object().pattern(
-        Joi.string(),
-        Joi.object({
-            text: Joi.string(),
-        })
-    ),
-    moderator: Joi.object({
-        name: Joi.string(),
-        email: Joi.string(),
-        message: Joi.string(),
-        invitations: Joi.array().items(
+    webComponent: {
+        webComponent: 'ElectionDoc',
+        electionName: Joi.string(),
+        organizationName: Joi.string(),
+        electionDate: Joi.string(),
+        questions: Joi.object().pattern(
+            Joi.string(),
             Joi.object({
-                _id: ID,
                 text: Joi.string(),
-                sentDate: Joi.string(),
-                acceptedDate: Joi.string(),
+                time: Joi.string(),
             })
         ),
-        submissions: Joi.array().items(
+        script: Joi.object().pattern(
+            Joi.string(),
             Joi.object({
-                _id: ID,
-                url: Joi.string(),
-                date: Joi.string(),
+                text: Joi.string(),
             })
         ),
-    }),
-    candidates: Joi.object().pattern(
-        Joi.string(),
-        Joi.object({
-            uniqueId: ID,
+        moderator: Joi.object({
             name: Joi.string(),
             email: Joi.string(),
-            office: Joi.string(),
-            region: Joi.string(),
+            message: Joi.string(),
             invitations: Joi.array().items(
                 Joi.object({
                     _id: ID,
@@ -70,62 +48,87 @@ const schema = Joi.object({
                     date: Joi.string(),
                 })
             ),
-        })
-    ),
-    timeline: {
-        moderatorDeadlineReminderEmails: Joi.object().pattern(
+        }),
+        candidates: Joi.object().pattern(
             Joi.string(),
             Joi.object({
-                date: Joi.string(),
-                sent: Joi.boolean(),
+                uniqueId: ID,
+                name: Joi.string(),
+                email: Joi.string(),
+                office: Joi.string(),
+                region: Joi.string(),
+                invitations: Joi.array().items(
+                    Joi.object({
+                        _id: ID,
+                        text: Joi.string(),
+                        sentDate: Joi.string(),
+                        acceptedDate: Joi.string(),
+                    })
+                ),
+                submissions: Joi.array().items(
+                    Joi.object({
+                        _id: ID,
+                        url: Joi.string(),
+                        date: Joi.string(),
+                    })
+                ),
             })
         ),
-        moderatorSubmissionDeadline: Joi.object().pattern(
-            Joi.string(),
-            Joi.object({
-                date: Joi.string(),
-                sent: Joi.boolean(),
-            })
-        ),
-        moderatorInviteDeadline: Joi.object().pattern(
-            Joi.string(),
-            Joi.object({
-                date: Joi.string(),
-                sent: Joi.boolean(),
-            })
-        ),
-        candidateDeadlineReminderEmails: Joi.object().pattern(
-            Joi.string(),
-            Joi.object({
-                date: Joi.string(),
-                sent: Joi.boolean(),
-            })
-        ),
-        candidateSubmissionDeadline: Joi.object().pattern(
-            Joi.string(),
-            Joi.object({
-                date: Joi.string(),
-                sent: Joi.boolean(),
-            })
-        ),
+        timeline: {
+            moderatorDeadlineReminderEmails: Joi.object().pattern(
+                Joi.string(),
+                Joi.object({
+                    date: Joi.string(),
+                    sent: Joi.boolean(),
+                })
+            ),
+            moderatorSubmissionDeadline: Joi.object().pattern(
+                Joi.string(),
+                Joi.object({
+                    date: Joi.string(),
+                    sent: Joi.boolean(),
+                })
+            ),
+            moderatorInviteDeadline: Joi.object().pattern(
+                Joi.string(),
+                Joi.object({
+                    date: Joi.string(),
+                    sent: Joi.boolean(),
+                })
+            ),
+            candidateDeadlineReminderEmails: Joi.object().pattern(
+                Joi.string(),
+                Joi.object({
+                    date: Joi.string(),
+                    sent: Joi.boolean(),
+                })
+            ),
+            candidateSubmissionDeadline: Joi.object().pattern(
+                Joi.string(),
+                Joi.object({
+                    date: Joi.string(),
+                    sent: Joi.boolean(),
+                })
+            ),
+        },
+        undebateDate: Joi.string(),
     },
-    undebateDate: Joi.string(),
 })
 
-export default async function upsertElectionDoc(doc, cb) {
+export default async function findAndSetElectionDoc(query, doc, cb) {
     if (!this.synuser) return cb && cb() // no user
     if (!doc._id) {
         logger.error('ElectionDoc must container an _id', doc)
         return cb && cb()
     }
-    const valid = schema.validate(doc)
+    const valid = schema.validate({ webComponent: doc })
     if (valid.error) {
         logger.error('ElectionDoc validation', valid)
         return cb && cb()
     }
     try {
         // upsert
-        await Iota.updateOne({ _id: doc._id }, { $set: doc }, { upsert: true })
+        await Iota.updateOne(query, { $set: { webComponent: doc } }, { upsert: true })
         cb && cb(true)
     } catch (err) {
         logger.error('upsertElectionDoc', err)
