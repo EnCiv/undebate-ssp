627d00da1ce726a35c423e4054407166eea00487
David Fridley <ddfridley@yahoo.com>
2022-02-21 10:29:21 -0800

    no consols

--- app/socket-apis/subscribe-election-info.js
+++ app/socket-apis/subscribe-election-info.js
@@ -12,19 +12,18 @@ export default function subscribeElectionInfo(id, cb) {
         const electionObj = await Iota.findOne({ _id: Iota.ObjectId(id) })
         socket.join(id) // join this user into the socket.io room related to this item
         function electionInfoUpdate(iota) {
-            console.info('electionInfoUpdate', iota)
-            if (iota.parentId !== id) return console.info("didn't match parentId")
+            if (iota.parentId !== id) return
             if (!electionObj.participants) electionObj.participants = []
             electionObj.participants.unshift(iota)
             const event = subscribeEventName('subscribe-election-info', id)
             socket.broadcast.to(id).emit(event, { participants: electionObj.participants })
             socket.emit(event, { participants: electionObj.participants })
         }
-        cb(electionObj)
         serverEvents.on(serverEvents.eNames.ParticipantCreated, electionInfoUpdate)
         socket.on('disconnect', () =>
             serverEvents.removeListener(serverEvents.eNames.ParticipantCreated, electionInfoUpdate)
         )
+        cb(electionObj)
     }
     // 'this' is a socket belonging to the API caller
     subscribeIt(this)
