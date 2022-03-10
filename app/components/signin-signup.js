cd8f7295ffe63b500075563c4573132041a024c7
gluay88 <p.wantagaew@gmail.com>
2022-03-10 14:08:04 -0800

    .stories.js page testing

---
+++ app/components/signin-signup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignInSingUp() {
+    return (
+        <>
+            <h1>Sign In and Sign Up page</h1>
+        </>
+    )
+}
+
+export default SignInSingUp
