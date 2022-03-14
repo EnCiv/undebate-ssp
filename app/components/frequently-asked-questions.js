ff9c39f8755bb5e562bf5f39a4cb0c653840e077
gitgood-ENT <ward.noahd@gmail.com>
2022-03-14 11:02:45 -0700

    added transtition effects

--- app/components/frequently-asked-questions.js
+++ app/components/frequently-asked-questions.js
@@ -4,57 +4,69 @@ const faqs = [
     {
         question: 'This is the first question?',
         answer: 'This is the answer to the first question.',
-        displayed: false,
     },
     {
         question: 'This is the second question?',
         answer: 'This is the answer to the second question.',
-        displayed: false,
     },
     {
         question: 'This is the third question?',
-        answer: 'This is the answer to the third question.',
-        displayed: false,
+        answer: 'This is the answer to the third question. The answer to this question is very long. There was a lot to talk about. It takes up a lot of space on the page. I wonder how this will look. It will probably look pretty good by the time it is finished but lets give it a nice start. What do you say?',
     },
     {
         question: 'This is the fourth question?',
         answer: 'This is the answer to the fourth question.',
-        displayed: false,
     },
     {
         question: 'This is the fifth question?',
         answer: 'This is the answer to the fifth question.',
-        displayed: false,
     },
 ]
+const headerStyle = { textAlign: 'center', paddingTop: '1rem' }
+
+const answerStyle = {
+    paddingLeft: '1rem',
+    paddingRight: '2rem',
+    fontWeight: '500',
+    paddingBottom: 'max(15px, .2rem)',
+    transition: 'all, .5s, ease-in-out, .5s',
+    maxHeight: 'max-content',
+}
+const answerHidden = {
+    fontSize: '0px',
+    paddingLeft: '1rem',
+    paddingRight: '2rem',
+    maxHeight: 'max-content',
+    transition: 'all, .5s, ease-in-out, 0s',
+}
+const questionLine = { display: 'inline' }
+
+const questionStyle = { paddingLeft: '1rem', fontWeight: '300', display: 'inline-block' }
+
+const Caret = { display: 'inline-block', float: 'right', paddingRight: '2rem' }
 
 export default function FrequentlyAskedQuestions() {
     const [active, setActive] = useState(false)
 
-    const toggleAnswer = () => {
-        setActive(false)
-    }
-
     return (
         <>
-            <h3>Frequently Asked Questions</h3>
+            <h3 style={headerStyle}>Frequently Asked Questions</h3>
             {faqs.map(value => (
                 <div>
-                    {console.log(active)}
-                    <div>
-                        <div>
-                            <p
-                                key={value.question}
-                                onClick={() => {
-                                    active === value.question ? setActive(false) : setActive(value.question)
-                                }}
-                            >
-                                {value.question}
-                            </p>
-                            <hr></hr>
-                        </div>
-                        <p>{active === value.question && value.answer}</p>
+                    <div
+                        style={questionLine}
+                        key={value.question}
+                        onClick={() => {
+                            active === value.question ? setActive(false) : setActive(value.question)
+                        }}
+                    >
+                        <p style={questionStyle}>{value.question}</p>
+                        {active === value.question ? <p style={Caret}>^</p> : <p style={Caret}>v</p>}
+                        <hr></hr>
                     </div>
+                    <p style={active === value.question ? answerStyle : answerHidden}>
+                        {active === value.question ? value.answer : ' '}
+                    </p>
                 </div>
             ))}
         </>
