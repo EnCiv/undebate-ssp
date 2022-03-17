c8f93fc9bd7a5d81746f9586358eea01cef575a8
gitgood-ENT <ward.noahd@gmail.com>
2022-03-17 15:53:50 -0700

    moved test variable to story, moved styling to usestyles and added transition effect, added props to component, added tests for empty input

--- app/components/frequently-asked-questions.js
+++ app/components/frequently-asked-questions.js
@@ -1,74 +1,80 @@
 import React, { useEffect, useRef, useState } from 'react'
+import { createUseStyles } from 'react-jss'
+import cx from 'classnames'
 
-const faqs = [
-    {
-        question: 'This is the first question?',
-        answer: 'This is the answer to the first question.',
-    },
-    {
-        question: 'This is the second question?',
-        answer: 'This is the answer to the second question.',
-    },
-    {
-        question: 'This is the third question?',
-        answer: 'This is the answer to the third question. The answer to this question is very long. There was a lot to talk about. It takes up a lot of space on the page. I wonder how this will look. It will probably look pretty good by the time it is finished but lets give it a nice start. What do you say?',
-    },
-    {
-        question: 'This is the fourth question?',
-        answer: 'This is the answer to the fourth question.',
-    },
-    {
-        question: 'This is the fifth question?',
-        answer: 'This is the answer to the fifth question.',
-    },
-]
-const headerStyle = { textAlign: 'center', paddingTop: '1rem' }
-
-const answerStyle = {
-    paddingLeft: '1rem',
-    paddingRight: '2rem',
-    fontWeight: '500',
-    paddingBottom: 'max(15px, .2rem)',
-    transition: 'all, .5s, ease-in-out, .5s',
-    maxHeight: 'max-content',
-}
-const answerHidden = {
-    fontSize: '0px',
-    paddingLeft: '1rem',
-    paddingRight: '2rem',
-    maxHeight: 'max-content',
-    transition: 'all, .5s, ease-in-out, 0s',
-}
-const questionLine = { display: 'inline' }
-
-const questionStyle = { paddingLeft: '1rem', fontWeight: '300', display: 'inline-block' }
-
-const Caret = { display: 'inline-block', float: 'right', paddingRight: '2rem' }
-
-export default function FrequentlyAskedQuestions() {
+export default function FrequentlyAskedQuestions(props) {
     const [active, setActive] = useState(false)
+    const { className, style, faqs } = props
+    const classes = useStyles()
 
     return (
-        <>
-            <h3 style={headerStyle}>Frequently Asked Questions</h3>
-            {faqs.map(value => (
-                <div>
-                    <div
-                        style={questionLine}
-                        key={value.question}
-                        onClick={() => {
-                            active === value.question ? setActive(false) : setActive(value.question)
-                        }}
-                    >
-                        <p style={questionStyle}>{value.question}</p>
-                        {active === value.question ? <p style={Caret}>^</p> : <p style={Caret}>v</p>}
+        <div>
+            <h3 className={cx(className, classes.headerStyle)} style={style}>
+                Frequently Asked Questions
+            </h3>
+            {faqs &&
+                faqs.map(value => (
+                    <div>
+                        <div
+                            className={cx(className, classes.questionLine)}
+                            style={style}
+                            key={value.question}
+                            onClick={() => {
+                                active === value.question ? setActive(false) : setActive(value.question)
+                            }}
+                        >
+                            <p className={cx(className, classes.questionStyle)} style={style}>
+                                {value.question}
+                            </p>
+                            {active === value.question ? (
+                                <p className={cx(className, classes.caret)} style={style}>
+                                    ^
+                                </p>
+                            ) : (
+                                <p className={cx(className, classes.caret)} style={style}>
+                                    v
+                                </p>
+                            )}
+                        </div>
+                        <div
+                            style={{
+                                overflow: active !== value.question ? 'hidden' : 'auto',
+                                maxHeight: active === value.question ? '500px' : '0px',
+                                transition: 'all 1s cubic-bezier(1,0,0,1)',
+                            }}
+                        >
+                            <p
+                                className={cx(className, classes.answerStyle)}
+                                style={style}
+
+                                // had conditional styling based on whether the answer is displayed or not. need to find out how to do proper transition effects.
+                                // style={active === value.question ? answerStyle : answerHidden}
+                            >
+                                {value.answer}
+                            </p>
+                        </div>
                         <hr></hr>
                     </div>
-                    <p style={active === value.question ? answerStyle : answerHidden}>
-                        {active === value.question ? value.answer : ' '}
-                    </p>
-                </div>
-            ))}
-        </>
+                ))}
+        </div>
     )
 }
+
+const useStyles = createUseStyles({
+    caret: { display: 'inline-block', float: 'right', paddingRight: '2rem' },
+
+    headerStyle: { textAlign: 'center', paddingTop: '1rem' },
+
+    answerStyle: {
+        paddingLeft: '1rem',
+        paddingRight: '2rem',
+        fontWeight: '500',
+        // used px here to maintain a minimum distance from the bottom of the page for ease of visibility
+        paddingBottom: 'max(15px, .2rem)',
+        // transition: 'all, .5s, ease-in-out, .1s',
+    },
+
+    questionLine: { display: 'inline' },
+
+    questionStyle: { paddingLeft: '1rem', fontWeight: '300', display: 'inline-block' },
+})
