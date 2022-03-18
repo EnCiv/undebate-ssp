9d5f21143996f206b6ed0873616ddac0dba72ab6
Punjabi2021 <93889575+Punjabi2021@users.noreply.github.com>
2022-03-18 10:29:22 -0700

    Set up the Icon.

--- app/components/icon-box.js
+++ app/components/icon-box.js
@@ -4,19 +4,32 @@ import { createUseStyles } from 'react-jss'
 import cx from 'classnames'
 
 import SvgFitsInside from '../svgr/fits-inside'
+import SvgAutomate from '../svgr/automate'
+import SvgShortFormat from '../svgr/short-format'
 const icons = {
     SvgFitsInside: SvgFitsInside,
+    SvgAutomate: SvgAutomate,
+    SvgShortFormat: SvgShortFormat,
 }
 
 function IconBox(props) {
     const { className, style, subject, description, iconName } = props
     const classes = useStyles()
     const Icon = icons[iconName] || SvgFitsInside
-
+    const Icon2 = icons[iconName] || SvgAutomate
+    const Icon3 = icons[iconName] || SvgShortFormat
     return (
-        <div className={cx(className, classes.background)} style={style}>
-            <Icon className={classes.userImage} />
-        </div>
+        <>
+            <div className='iconBox'>
+                <div className={cx(className, classes.background)} style={style}>
+                    <Icon />
+                </div>
+                <div>
+                    <h2>{subject}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+        </>
     )
 }
 
--- stories/icon-box.stories.js
+++ stories/icon-box.stories.js
@@ -15,14 +15,21 @@ Default.args = {}
 
 export const TestOne = Template.bind({})
 TestOne.args = {
-    subject: 'Test One',
-    description: 'Just one Test',
+    subject: 'Fits inside any layout, Embed anywhere!',
+    description: 'Embeds available for multiple layouts. Delightful experience across a variety of devices. ',
     iconName: 'SvgFitsInside',
 }
 
-export const Background = Template.bind({})
-Background.args = {
-    style: {
-        background: 'lightgrey',
-    },
+export const TestTwo = Template.bind({})
+TestTwo.args = {
+    subject: 'Automated and structured process.',
+    description: 'Invites, reminders and scripts automated to ease the participation of all entities involved.',
+    iconName: 'SvgAutomate',
+}
+
+export const TestThree = Template.bind({})
+TestThree.args = {
+    subject: 'Short formats to decide quickly.',
+    description: 'Time conciousness at every step that caters more towards the modern attention span.',
+    iconName: 'SvgShortFormat',
 }
