f4cb75aad11a2c2bd80b8e8c2f9c7f229afce195
Isaac Hudson <ice1080@gmail.com>
2022-03-20 14:56:10 -0600

    adding in storybook interactions and testing library

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 
 module.exports = {
     stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+    addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
     framework: '@storybook/react',
     core: {
         builder: 'webpack5',
--- stories/upload-csv.stories.js
+++ stories/upload-csv.stories.js
@@ -1,19 +1,35 @@
 // https://github.com/EnCiv/undebate-ssp/issues/54
 import React, { useEffect } from 'react'
+import { within, userEvent } from '@storybook/testing-library'
 import UploadCSV from '../app/components/upload-csv'
 
 export default {
     title: 'Upload CSV',
     component: UploadCSV,
+    decorators: [
+        Story => (
+            <div style={{ marginLeft: '15rem' }}>
+                {/* marginLeft so that popup doesn't move button to the right when clicked */}
+                <Story />
+            </div>
+        ),
+    ],
 }
 
 const Template = (args, context) => {
-    const { onDone, electionOM } = context
+    const { electionOM } = context
     const { defaultValue, ...otherArgs } = args
     const [electionObj, electionMethods] = electionOM
     useEffect(() => defaultValue && electionMethods.upsert(defaultValue), [defaultValue])
 
-    return <UploadCSV electionOM={electionOM} {...otherArgs} onDone={onDone} />
+    return <UploadCSV electionOM={electionOM} {...otherArgs} />
 }
 
 export const Default = Template.bind({})
+
+export const Clicked = Template.bind({})
+Clicked.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    await userEvent.click(canvas.getByRole('button'))
+}
