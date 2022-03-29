1291c01a6a51192c120308499e43e122bd64af3e
gluay88 <p.wantagaew@gmail.com>
2022-03-29 14:07:20 -0700

    iotas.json added

---
+++ app/web-components/undebates.js
@@ -0,0 +1,18 @@
+// https://github.com/EnCiv/undebate-ssp/issues/109
+'use strict'
+
+import React from 'react'
+
+import UndebatesCom from '../components/undebates'
+
+function Undebates(props) {
+    return (
+        <>
+            <div>
+                <UndebatesCom />
+            </div>
+        </>
+    )
+}
+
+export default Undebates
