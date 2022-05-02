1fb69611b0043c3408a3fb9d993698a781f77cf6
Isaac Hudson <ice1080@gmail.com>
2022-05-01 18:51:27 -0600

    start on google api, will have to refactor to a backend call eventually

--- app/components/paste-google-sheets-popup.js
+++ app/components/paste-google-sheets-popup.js
@@ -1,20 +1,167 @@
 // https://github.com/EnCiv/undebate-ssp/issues/55
-import React, { useState, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import { createUseStyles } from 'react-jss'
 import cx from 'classnames'
 import _ from 'lodash'
 import ExternalLinkSvg from '../svgr/external-link'
 import LinkSvg from '../svgr/link'
 
+// todo change all of this to backend calls so that the api key is not exposed.
+// todo add to documentation for env
+// todo change all google auth2 usages to newer libraries
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+
+const loadSheetsApi = callback => {
+    const existingScript = document.getElementById('sheets-api-script')
+
+    if (!existingScript) {
+        const script = document.createElement('script')
+        script.src = 'https://apis.google.com/js/api.js'
+        script.id = 'sheets-api-script'
+        script.async = true
+        script.defer = true
+        document.body.appendChild(script)
+
+        script.onload = () => {
+            if (callback) callback()
+        }
+    }
+
+    if (existingScript && callback) callback()
+}
+
 function PasteGoogleSheetsPopup({ electionObj, electionMethods, closePopup, visible, className, style = {} }) {
+    const GENERAL_ERROR = 'Unable to extract data from link. Please compare your document with the sample document.'
+    const LINK_NOT_FOUND_ERROR = 'Unable to find a Google Sheets Document at the below link.'
+    const MISSING_AUTH_ERROR = 'Auth missing. Please open all access to your Google doc.'
+    const UNAUTHORIZED_ERROR =
+        'Could not authenticate Google Spreadsheets. Please allow access in the popup when clicking Extract Data.'
+    const NO_DATA_FOUND_ERROR = 'No data found in sheet. Does data exist in range ' + SHEET_VALUES_RANGE + '?'
+    const REQUIRED_COLUMNS = ['name', 'email', 'office']
+    const SHEETS_API_BASE = 'https://sheets.googleapis.com/v4/spreadsheets/'
+    const SHEET_VALUES_RANGE = 'A:ZZ'
+    const GOOGLE_AUTH_SCOPE = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+
     const classes = useStyles()
     const [inputLink, setInputLink] = useState('')
     const [fileError, setFileError] = useState(null)
+    let GoogleAuth
+    let isAuthorized = false
+
+    useEffect(() => {
+        loadSheetsApi(() => {
+            gapi.load('client', initGapiClient)
+        })
+    }, [])
+
+    const initGapiClient = () => {
+        // todo wait till loaded and inited to enable extract data button
+        console.log('initializing gapi client', GOOGLE_API_KEY)
+        gapi.client.init({
+            apiKey: GOOGLE_API_KEY,
+            clientId: GOOGLE_CLIENT_ID,
+            scope: GOOGLE_AUTH_SCOPE,
+            discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+        })
+    }
 
     const onInputChange = event => {
         event.preventDefault()
         setInputLink(event.target.value)
         setFileError(null)
+    }
+
+    const linkIsGoogleSheets = () => {
+        return inputLink.match(/[-\w]{25,}/)
+    }
+
+    const handleSheetData = data => {
+        console.log('handleSheetData', data)
+        // todo
+    }
+
+    const getSheetData = spreadsheetId => {
+        // assumes you are already signed in for this sheet
+        console.log('spreadsheetId:', spreadsheetId)
+        console.log(gapi)
+        gapi.client.sheets.spreadsheets.values
+            .get({
+                spreadsheetId: spreadsheetId,
+                range: SHEET_VALUES_RANGE,
+            })
+
+            .then(response => {
+                console.log('response', response)
+                if (response.status === 403) {
+                    setFileError(MISSING_AUTH_ERROR)
+                } else {
+                    const json = response.result
+                    console.log('json', json)
+                    // todo handle missing values here
+                    json && handleSheetData(json.values)
+                }
+            })
+            .catch(error => {
+                console.error('uh oh', error.body)
+                setFileError(GENERAL_ERROR)
+            })
+    }
+
+    const handleValidSheet = () => {
+        // assumes all data is in the range A:ZZ
+        const spreadsheetId = inputLink.match(/[-\w]{25,}/)[0]
+
+        console.log('testing authorization: ', isAuthorized)
+        if (isAuthorized) {
+            getSheetData(spreadsheetId)
+        } else {
+            GoogleAuth = gapi.auth2.getAuthInstance()
+
+            // consider this instead of signIn: requestAccessToken
+            // https://developers.google.com/sheets/api/quickstart/js
+            GoogleAuth.signIn({ scope: GOOGLE_AUTH_SCOPE }).then(response => {
+                console.log('sign in response', response)
+                if (GoogleAuth.isSignedIn.get()) {
+                    isAuthorized = true
+
+                    getSheetData(spreadsheetId)
+                } else {
+                    isAuthorized = false
+                    setFileError(UNAUTHORIZED_ERROR)
+                }
+            })
+        }
+    }
+
+    const handleExtractClick = event => {
+        event.preventDefault()
+        setFileError(null)
+        if (linkIsGoogleSheets()) {
+            try {
+                handleValidSheet()
+            } catch (exc) {
+                console.error('general error: ', exc)
+                setFileError(GENERAL_ERROR)
+            }
+        } else {
+            setFileError(LINK_NOT_FOUND_ERROR)
+        }
+    }
+
+    const isValidUrl = () => {
+        let url
+        try {
+            url = new URL(inputLink)
+        } catch (e) {
+            return false
+        }
+
+        return url.protocol === 'https:'
+    }
+
+    const isExtractButtonEnabled = () => {
+        return inputLink && isValidUrl(inputLink)
     }
 
     const renderErrors = () => {
@@ -28,14 +175,17 @@ function PasteGoogleSheetsPopup({ electionObj, electionMethods, closePopup, visi
     const renderPasteLink = () => {
         return (
             <div className={classes.inputLinkBox}>
-                <input className={classes.inputLink} type='text' value={inputLink} onChange={onInputChange} />
+                <input
+                    id='sheets-link'
+                    data-testid='sheets-link'
+                    className={classes.inputLink}
+                    type='text'
+                    value={inputLink}
+                    onChange={onInputChange}
+                />
                 <LinkSvg className={classes.linkIcon} />
             </div>
         )
-    }
-
-    const handleExtractClick = event => {
-        event.preventDefault()
     }
 
     return (
@@ -49,7 +199,7 @@ function PasteGoogleSheetsPopup({ electionObj, electionMethods, closePopup, visi
                     <span className={classes.provideText}>Provide Election Table</span>
                     <div className={classes.checkSampleRow}>
                         <div>Paste Google Sheets Link</div>
-                        <a href='/assets/csv/sample-candidate-table.csv' className={classes.checkSampleText}>
+                        <a href='todo' className={classes.checkSampleText}>
                             Check Sample &nbsp;
                             <ExternalLinkSvg className={classes.externalLinkIcon} />
                         </a>
@@ -64,8 +214,12 @@ function PasteGoogleSheetsPopup({ electionObj, electionMethods, closePopup, visi
                     <button
                         id='extract-data-button'
                         type='button'
-                        disabled={!inputLink}
-                        className={cx(classes.btn, classes.extractButton, !inputLink && classes.disabledExtractButton)}
+                        disabled={!isExtractButtonEnabled()}
+                        className={cx(
+                            classes.btn,
+                            classes.extractButton,
+                            !isExtractButtonEnabled() && classes.disabledExtractButton
+                        )}
                         onClick={handleExtractClick}
                     >
                         Extract Data
--- stories/paste-google-sheets-popup.stories.js
+++ stories/paste-google-sheets-popup.stories.js
@@ -1,7 +1,12 @@
 // https://github.com/EnCiv/undebate-ssp/issues/55
 import React, { useEffect } from 'react'
-import { within, userEvent } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { getElectionCandidates } from './story-helpers'
 import PasteGoogleSheetsPopup from '../app/components/paste-google-sheets-popup'
+
+const happyPathLink =
+    'https://docs.google.com/spreadsheets/d/1K0qt8A25qTVocoVbzVPUEnMRVvEaiq0cE86WmqShRKI/edit?usp=sharing'
 
 export default {
     title: 'Paste Google Sheets Popup',
@@ -27,3 +32,22 @@ const Template = (args, context) => {
 
 export const Default = Template.bind({})
 Default.args = { defaultValue: {} }
+Default.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    // ensure button is disabled by default:
+    await waitFor(() => expect(canvas.getByText('Extract Data').getAttribute('disabled')).toBe(''))
+}
+
+export const HappyPath = Template.bind({})
+HappyPath.args = { defaultValue: {} }
+HappyPath.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+    await new Promise(r => setTimeout(r, 1000))
+
+    await waitFor(() => userEvent.type(canvas.getByTestId('sheets-link'), happyPathLink))
+    await new Promise(r => setTimeout(r, 1000))
+
+    await userEvent.click(canvas.getByText('Extract Data'))
+    // todo
+}
