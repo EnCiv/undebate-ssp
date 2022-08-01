79a13205c0040320482ea714a7e84fd50c6fd9d4
David Fridley <ddfridley@yahoo.com>
2022-07-31 18:03:47 -0700

    background color and minHeight of page

--- app/components/undebate-homepage.js
+++ app/components/undebate-homepage.js
@@ -69,6 +69,7 @@ export default function UndebateHomepage(props) {
 
 const useStyles = createUseStyles(theme => ({
     undebateHomePage: {
+        minHeight: '75vh',
         backgroundColor: theme.backgroundColorApp,
     },
     header: {},
