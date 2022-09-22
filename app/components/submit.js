269e8fcb5f083de249be078aa24a296c7a7ff8c6
mattwinne <matt.winne@gmail.com>
2022-09-21 20:00:00 -0700

    add user feedback between button click and response

--- app/components/submit.js
+++ app/components/submit.js
@@ -5,28 +5,38 @@
 import cx from 'classnames'
 import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
+import Spinner from './spinner'
 
 function Submit({ onDone, name = 'Submit', style, className, disabled = false, disableOnClick = false }) {
     const classes = useStyles()
     const [disabledAfterClick, setDisabledAfterClick] = useState(false)
+    const [awaitingResponse, setAwaitingResponse] = useState(false)
 
-    const handleOnClick = () => {
+    const handleOnClick = async () => {
+        setAwaitingResponse(true)
         if (!disabled && !disabledAfterClick) {
-            onDone({ valid: true })
+            await onDone({ valid: true })
         }
         if (disableOnClick) setDisabledAfterClick(true)
+        setAwaitingResponse(false)
     }
 
     return (
-        <button
-            type='button'
-            className={cx(className, classes.btn, (disabled || disabledAfterClick) && classes.disabled)}
-            style={style}
-            onClick={handleOnClick}
-            disabled={disabled}
-        >
-            {name}
-        </button>
+        <>
+            {!awaitingResponse ? (
+                <button
+                    type='button'
+                    className={cx(className, classes.btn, (disabled || disabledAfterClick) && classes.disabled)}
+                    style={style}
+                    onClick={handleOnClick}
+                    disabled={disabled}
+                >
+                    {name}
+                </button>
+            ) : (
+                <Spinner style={{ justifyContent: 'right', paddingRight: '1rem' }} />
+            )}
+        </>
     )
 }
 
