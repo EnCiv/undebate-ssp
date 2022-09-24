5cd59c556fb4aa82f3852f1649a244a9be3846cc
mattwinne <matt.winne@gmail.com>
2022-09-24 12:24:00 -0700

    display spinner between call and completion of createModeratorRecorder and sendModeratorInvitation functions

--- app/components/submit.js
+++ app/components/submit.js
@@ -5,38 +5,28 @@
 import cx from 'classnames'
 import React, { useState } from 'react'
 import { createUseStyles } from 'react-jss'
-import Spinner from './spinner'
 
 function Submit({ onDone, name = 'Submit', style, className, disabled = false, disableOnClick = false }) {
     const classes = useStyles()
     const [disabledAfterClick, setDisabledAfterClick] = useState(false)
-    const [awaitingResponse, setAwaitingResponse] = useState(false)
 
-    const handleOnClick = async () => {
-        setAwaitingResponse(true)
+    const handleOnClick = () => {
         if (!disabled && !disabledAfterClick) {
-            await onDone({ valid: true })
+            onDone({ valid: true })
         }
         if (disableOnClick) setDisabledAfterClick(true)
-        setAwaitingResponse(false)
     }
 
     return (
-        <>
-            {!awaitingResponse ? (
-                <button
-                    type='button'
-                    className={cx(className, classes.btn, (disabled || disabledAfterClick) && classes.disabled)}
-                    style={style}
-                    onClick={handleOnClick}
-                    disabled={disabled}
-                >
-                    {name}
-                </button>
-            ) : (
-                <Spinner style={{ justifyContent: 'right', paddingRight: '1rem' }} />
-            )}
-        </>
+        <button
+            type='button'
+            className={cx(className, classes.btn, (disabled || disabledAfterClick) && classes.disabled)}
+            style={style}
+            onClick={handleOnClick}
+            disabled={disabled}
+        >
+            {name}
+        </button>
     )
 }
 
