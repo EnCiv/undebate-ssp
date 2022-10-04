78ca327543437fea0f3424415b3c8a5ea509fc0a
mattwinne <matt.winne@gmail.com>
2022-10-03 20:46:24 -0700

    Include user metadata in file upload

--- app/socket-apis/cloudinary-upload.js
+++ app/socket-apis/cloudinary-upload.js
@@ -6,7 +6,12 @@ export default async function cloudinaryUpload(file, cb) {
         return cb && cb() // no user
     }
     try {
-        const result = await cloudinary.v2.uploader.upload(file)
+        const options = {
+            context: {
+                user: this.synuser.id,
+            },
+        }
+        const result = await cloudinary.v2.uploader.upload(file, options)
         cb({ url: result.secure_url })
     } catch (error) {
         cb({ error: error })
