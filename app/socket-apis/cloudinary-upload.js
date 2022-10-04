ead81edca48d1ed01690042b268f9845d13d2a3c
mattwinne <matt.winne@gmail.com>
2022-10-03 20:29:11 -0700

    Require user login for api call

--- app/socket-apis/cloudinary-upload.js
+++ app/socket-apis/cloudinary-upload.js
@@ -1,6 +1,10 @@
 import cloudinary from 'cloudinary'
 
 export default async function cloudinaryUpload(file, cb) {
+    if (!this.synuser || !this.synuser.id) {
+        logger.error('cloudinaryUpload called but no user logged in')
+        return cb && cb() // no user
+    }
     try {
         const result = await cloudinary.v2.uploader.upload(file)
         cb({ url: result.secure_url })
